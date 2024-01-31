1bc96c0abe58c374c3cbc669a761a57a1614c579
chriskinal <118364224+chriskinal@users.noreply.github.com>
2024-01-31 06:06:23 -0600

    Led text fixes.

--- src/Autosteer_gps_teensy_v4_1.ino
+++ src/Autosteer_gps_teensy_v4_1.ino
@@ -72,10 +72,10 @@ uint32_t READ_BNO_TIME = 0;   //Used stop BNO data pile up (This version is with
 
 //Status LED's
 #define GGAReceivedLED 13         //Teensy onboard LED
-#define Power_on_LED 5            //Green
-#define Ethernet_Active_LED 6     //Blue
-#define GPSRED_LED 9              //Yellow (Flashing = NO IMU or Dual, ON = GPS fix with IMU)
-#define GPSGREEN_LED 10           //Red (Flashing = Dual bad, ON = Dual good)
+#define Power_on_LED 5            //Red
+#define Ethernet_Active_LED 6     //Green
+#define GPSRED_LED 9              //Red (Flashing = NO IMU or Dual, ON = GPS fix with IMU)
+#define GPSGREEN_LED 10           //Green (Flashing = Dual bad, ON = Dual good)
 #define AUTOSTEER_STANDBY_LED 11  //Red
 #define AUTOSTEER_ACTIVE_LED 12   //Green
 uint32_t gpsReadyTime = 0;        //Used for GGA timeout
